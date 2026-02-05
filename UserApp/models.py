56710a8da2b807dc9f056c529990a0a329ca87c6
Sagar Khatri <khatrisagar2@gmail.com>
2026-02-05 11:23:44 +0530

    ModelChanges

--- UserApp/models.py
+++ UserApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+genderChoice = {
+    "male" : "male",
+    "female" : "female"
+}
+
 # Create your models here.
+class UserModel(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    contact = models.BigIntegerField(max_length=10)
+    password = models.CharField(max_length=20)
+    #gender = models.Choices(genderChoice)
+
+    def __str__(self):
+        return self.email
