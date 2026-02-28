fefe8c1ae2f67a56b75599460224443feec87417
Sagar Khatri <khatrisagar2@gmail.com>
2026-02-28 11:22:42 +0530

    Model Create Products

--- UserApp/admin.py
+++ UserApp/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
-from .models import UserModel
+from .models import *
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
     list_display = ["name","email","contact","password","gender"]
 
 admin.site.register(UserModel,UserAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id","name","image"]
+admin.site.register(CategoryModel,CategoryAdmin)
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ["id","category","name","image"]
+admin.site.register(SubCategoryModel,SubCategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["id","category","subCategory","name","image","price","discountType","discount"]
+admin.site.register(ProductModel,ProductAdmin)
--- UserApp/models.py
+++ UserApp/models.py
@@ -5,6 +5,11 @@ genderChoice = (
     ("Female" , "Female")
 )
 
+discountChoice = (
+    ("%","%"),
+    ("Rs","Rs")
+)
+
 # Create your models here.
 class UserModel(models.Model):
     name = models.CharField(max_length=50)
@@ -16,3 +21,31 @@ class UserModel(models.Model):
 
     def __str__(self):
         return self.email
+    
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="category/",default="")
+
+    def __str__(self):
+        return self.name
+    
+class SubCategoryModel(models.Model):
+    category = models.ForeignKey(CategoryModel,on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="subCategory/",default="")
+
+    def __str__(self):
+        return self.name
+    
+class ProductModel(models.Model):
+    category = models.ForeignKey(CategoryModel,on_delete=models.CASCADE)
+    subCategory = models.ForeignKey(SubCategoryModel,on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to="product/",default="")
+    price = models.BigIntegerField(max_length=10)
+    discountType = models.CharField(max_length=6,choices=discountChoice,default="%")
+    discount = models.BigIntegerField(max_length=10)
+
+    def __str__(self):
+        return self.name
